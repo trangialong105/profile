8a227eb8b99dde9880ae9cfb6b72e1b0fc86edef
trangialong105 <trangialong105@gmail.com>
2024-06-20 22:36:48 -0600

    First deploy

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className='px-32 py-24 text-center text-2xl'>
       {t(
-        'This_is_a_simple_about_page_just_to_show_how_you_can_use_the_internationalization_in_different_pages'
+        'Gia Long Tran is a business that manage online digital project from Vietnam. We are currently based in Canada'
       )}
     </div>
   )
